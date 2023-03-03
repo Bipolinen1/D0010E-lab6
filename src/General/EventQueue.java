94f52b3b01079d39e8d814337aae4aa38f8ff5c1
Bipolinen1 <hamben-2@student.ltu.se>
2023-03-03 14:21:54 +0100

    Ändrade EventQueue

--- src/General/EventQueue.java
+++ src/General/EventQueue.java
@@ -7,9 +7,7 @@ public class EventQueue extends ArrayList<Event>{
         super();
 
     }
-    public void something (){
 
-    }
     public void addEvent(Event event){
         for(int i = this.size() - 1; i >= 0; i--){
             if(event.getEventTime() < this.get(i).getEventTime()){
