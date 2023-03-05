f841134cc15bc6305ec5f47cbc1c7d97669353ba
Bipolinen1 <hamben-2@student.ltu.se>
2023-03-05 20:13:25 +0100

    La till några metoder i State

--- src/General/State.java
+++ src/General/State.java
@@ -4,9 +4,23 @@ import java.util.Observable;
 
 public class State extends Observable{
     private double currentTime;
-    private boolean running;
+    private boolean stopped;
+
+    public State(){}
 
     public double getCurrentTime(){
         return currentTime;
     }
+
+    public void setCurrentTime(double time){
+        if(time < this.getCurrentTime()){
+            throw new RuntimeException("Can't go back in time");
+        }
+        currentTime = time;
+    }
+
+    public void stop(){
+        stopped = true;
+    }
+
 }
