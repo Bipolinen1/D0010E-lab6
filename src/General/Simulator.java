1aa5a9305b085e8de2072dbf2a45da1b7afe34f6
Bipolinen1 <hamben-2@student.ltu.se>
2023-03-06 16:04:00 +0100

    Skrev Simulator

--- src/General/Simulator.java
+++ src/General/Simulator.java
@@ -1,8 +1,18 @@
 package General;
 
 public class Simulator {
+    protected EventQueue eventQueue;
+    protected State state;
+    public Simulator(EventQueue eventQueue, State state){
+        this.eventQueue = eventQueue;
+        this.state = state;
+    }
     public void run(){
-
+        while(!state.isStopped()){
+            Event event = eventQueue.get(0);
+            eventQueue.remove(0);
+            event.execute();
+        }
     }
 
 }
