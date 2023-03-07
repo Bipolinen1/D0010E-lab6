11679bcf662c1483067a47b3ed8b444e9002f5ae
Bipolinen1 <hamben-2@student.ltu.se>
2023-03-07 11:37:29 +0100

    La till några variabler

--- src/Supermarket/States/SupermarketState.java
+++ src/Supermarket/States/SupermarketState.java
@@ -3,18 +3,25 @@ package Supermarket.States;
 import General.State;
 
 public class SupermarketState extends State {
-    final int maxCustomers = 100;
-    final int maxOpenRegister = 10;
-    final double openingTime = 00.00;
-    final double closingTime = 22.00;
-    private int customersInStore;
-    private int customersInQueue;
+    final int maxCustomers;
+    final int maxOpenRegisters;
+    final double openingTime;
+    final double closingTime;
+    private int customersInStore = 0;
+    private int customersInQueue = 0;
     private CheckoutQueue checkoutQueue;
     private int payedCustomers = 0;
     private int missedCustomers = 0;
     private int openRegisters;
+    private double unUsedRegisterTime = 0;
+    private double totalQueueTime = 0;
 
-    public SupermarketState(){
+
+    public SupermarketState(int maxCustomers, int maxOpenRegisters, double openingTime, double closingTime){
         super();
+        this.maxCustomers = maxCustomers;
+        this.maxOpenRegisters = maxOpenRegisters;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
     }
 }
