33c4d521091a4da3336d32c14006593675ad0e9e
Bipolinen1 <hamben-2@student.ltu.se>
2023-03-12 17:31:46 +0100

    Skrev optimize metod3 och la till K.java som används vid test

---
+++ src/Supermarket/K.java
@@ -0,0 +1,105 @@
+package Supermarket;
+
+public interface K {
+
+    // Ex 1: (som sim1)
+//    public static final int M = 5;
+//    public static final double L = 1;
+//  public static final double LOW_COLLECTION_TIME = 0.5d;
+//    public static final double HIGH_COLLECTION_TIME = 1d;
+
+//    public static final double LOW_PAYMENT_TIME = 2d;
+//    public static final double HIGH_PAYMENT_TIME = 3d;
+
+//    public static final int SEED = 1234;
+//    public static final double END_TIME = 10.0d;
+//    public static final double STOP_TIME = 999.0d;
+
+
+    // Ex 2:
+//	  public static final int M = 7;
+//	  public static final double L = 2;
+//
+//	  public static final double LOW_COLLECTION_TIME = 0.5d;
+//	  public static final double HIGH_COLLECTION_TIME = 1d;
+//
+//	  public static final double LOW_PAYMENT_TIME = 2d;
+//	  public static final double HIGH_PAYMENT_TIME = 3d;
+//
+//	  public static final int SEED = 1234;
+//	  public static final double END_TIME = 10.0d;
+//	  public static final double STOP_TIME = 999.0d;
+
+    // Ex 3: (som sim2)
+//  public static final int M = 7;
+//  public static final double L = 3;
+//
+//  public static final double LOW_COLLECTION_TIME = 0.6d;
+//  public static final double HIGH_COLLECTION_TIME = 0.9d;
+//
+//  public static final double LOW_PAYMENT_TIME = 0.35d;
+//  public static final double HIGH_PAYMENT_TIME = 0.6d;
+//
+//  public static final int SEED = 13;
+//  public static final double END_TIME = 8.0d;
+//  public static final double STOP_TIME = 999.0d;
+
+    // Ex 4
+//    public static final int M = 100;
+//    public static final double L = 50;
+//
+//    public static final double LOW_COLLECTION_TIME = 0.45d;
+//    public static final double HIGH_COLLECTION_TIME = 0.65d;
+//
+//    public static final double LOW_PAYMENT_TIME = 0.2d;
+//    public static final double HIGH_PAYMENT_TIME = 0.3d;
+//
+//    public static final int SEED = 42;
+//    public static final double END_TIME = 20.0d;
+//    public static final double STOP_TIME = 999.0d;
+
+    // Ex 5
+
+    public static final int M = 1400;
+    public static final double L = 100;
+
+    public static final double LOW_COLLECTION_TIME = 0.45d;
+    public static final double HIGH_COLLECTION_TIME = 0.65d;
+
+    public static final double LOW_PAYMENT_TIME = 0.2d;
+    public static final double HIGH_PAYMENT_TIME = 0.3d;
+
+    public static final int SEED = 42;
+    public static final double END_TIME = 20.0d;
+    public static final double STOP_TIME = 999.0d;
+
+// Ex 6
+
+//    public static final int M = 1400;
+//    public static final double L = 700;
+//
+//    public static final double LOW_COLLECTION_TIME = 0.45d;
+//    public static final double HIGH_COLLECTION_TIME = 0.65d;
+//
+//    public static final double LOW_PAYMENT_TIME = 0.2d;
+//    public static final double HIGH_PAYMENT_TIME = 0.3d;
+//
+//    public static final int SEED = 42;
+//    public static final double END_TIME = 20.0d;
+//    public static final double STOP_TIME = 999.0d;
+
+// Ex 7
+//    public static final int M = 1400;
+//    public static final double L = 2000;
+//
+//    public static final double LOW_COLLECTION_TIME = 0.45d;
+//    public static final double HIGH_COLLECTION_TIME = 0.65d;
+//
+//    public static final double LOW_PAYMENT_TIME = 0.2d;
+//    public static final double HIGH_PAYMENT_TIME = 0.3d;
+//
+//    public static final int SEED = 42;
+//    public static final double END_TIME = 20.0d;
+//    public static final double STOP_TIME = 999.0d;
+
+}
--- src/Supermarket/Main/Optimize.java
+++ src/Supermarket/Main/Optimize.java
@@ -8,8 +8,12 @@ import General.Simulator;
 import Supermarket.Events.CloseEvent;
 import Supermarket.Events.EndEvent;
 import Supermarket.Events.StartEvent;
+import Supermarket.K;
 import Supermarket.States.SupermarketState;
 import Supermarket.View.SupermarketView;
+
+import java.awt.*;
+import java.util.Random;
 
 public class Optimize {
      /**
@@ -17,9 +21,12 @@ public class Optimize {
       * @param args
       */
      public static void main(String [] args){
-          int result = findMinRegistersSeed(0, 8.00, 7, 3.0,
-                  13, 0.35, 0.6, 0.6, 0.9);
-          System.out.println("Smallest amount of registers yielding least missed customers: "  + result);
+          /*int result = findMinRegistersSeed(0, K.END_TIME, K.STOP_TIME, K.M, K.L,
+                  K.SEED, K.LOW_PAYMENT_TIME, K.HIGH_PAYMENT_TIME, K.LOW_COLLECTION_TIME, K.HIGH_COLLECTION_TIME);
+          System.out.println("Smallest amount of registers yielding least missed customers: "  + result);*/
+          int result = findMinRegisters(0, K.END_TIME, K.STOP_TIME, K.M, K.L,
+                  K.LOW_PAYMENT_TIME, K.HIGH_PAYMENT_TIME, K.LOW_COLLECTION_TIME, K.HIGH_COLLECTION_TIME);
+          System.out.println(result);
      }
 
      /**
@@ -37,7 +44,7 @@ public class Optimize {
       * @return the state
       */
      public static SupermarketState runSimulationOnce(
-             double openingTime, double closingTime, int maxCustomers, int maxOpenRegisters, double lambda,
+             double openingTime, double closingTime, double stopTime, int maxCustomers, int maxOpenRegisters, double lambda,
              long seed, double kMin, double kMax, double pMin, double pMax){
           // Creates an EventQueue
           EventQueue eventQueue = new EventQueue();
@@ -55,7 +62,7 @@ public class Optimize {
           // Adds start, closing and end events
           eventQueue.addEvent(new StartEvent(eventQueue, openingTime, state));
           eventQueue.addEvent(new CloseEvent(eventQueue, closingTime, state));
-          eventQueue.addEvent(new EndEvent(eventQueue, 999, state));
+          eventQueue.addEvent(new EndEvent(eventQueue, stopTime, state));
 
           // Creates a instance of simulator
           Simulator simulator = new Simulator(eventQueue, state);
@@ -76,22 +83,22 @@ public class Optimize {
       * @param pMax maximum pick time
       * @return the amount of registers yielding least missed customers
       */
-     public static int findMinRegistersSeed(double openingTime, double closingTime, int maxCustomers,
+     public static int findMinRegistersSeed(double openingTime, double closingTime, double stopTime, int maxCustomers,
                                                   double lambda, long seed, double kMin, double kMax, double pMin,
                                                   double pMax) {
           int counter = 0;
           int numberOfRegisters = 0;
           int bestAmountOfRegisters = 0;
-          int lowestMissed = runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+          int lowestMissed = runSimulationOnce(openingTime, closingTime, stopTime, maxCustomers, numberOfRegisters,
                   lambda, seed, kMin, kMax, pMin, pMax).getMissedCustomers();
           // Loops until same result 10 times
           while(counter < 10){
                // updates lowestMissed, bestAmountOfRegister and counter if the simulation yields a better result
-               if(runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+               if(runSimulationOnce(openingTime, closingTime, stopTime, maxCustomers, numberOfRegisters,
                        lambda, seed, kMin, kMax, pMin, pMax).getMissedCustomers() < lowestMissed){
-                    lowestMissed = runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+                    lowestMissed = runSimulationOnce(openingTime, closingTime, stopTime, maxCustomers, numberOfRegisters,
                             lambda, seed, kMin, kMax, pMin, pMax).getMissedCustomers();
-                    bestAmountOfRegisters = runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+                    bestAmountOfRegisters = runSimulationOnce(openingTime, closingTime, stopTime, maxCustomers, numberOfRegisters,
                             lambda, seed, kMin, kMax, pMin, pMax).getOpenRegisters();
                     counter = 0;
                }
@@ -103,7 +110,27 @@ public class Optimize {
           return bestAmountOfRegisters;
      }
 
-     public static int findMinRegisters(){
+     public static int findMinRegisters(double openingTime, double closingTime, double stopTime, int maxCustomers,
+                                        double lambda, double kMin, double kMax, double pMin,
+                                        double pMax){
+          int counter = 0;
+          Random rand = new Random();
+          int minRegisters = findMinRegistersSeed(openingTime, closingTime, stopTime, maxCustomers,
+                  lambda, rand.nextInt(), kMin, kMax, pMin, pMax);
 
+          while(counter < 100){
+               int currentRegisters = findMinRegistersSeed(openingTime, closingTime, stopTime, maxCustomers,
+                       lambda, rand.nextInt(), kMin, kMax, pMin, pMax);
+               if(currentRegisters <= minRegisters){
+                    counter++;
+                    System.out.println(counter);
+               }
+               else{
+                    System.out.println(minRegisters);
+                    minRegisters = currentRegisters;
+                    counter = 0;
+               }
+          }
+          return minRegisters;
      }
 }
