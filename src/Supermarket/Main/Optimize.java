eeda91272ee22b61f5f24bc7e9d83e9bb7004f7a
Bipolinen1 <hamben-2@student.ltu.se>
2023-03-12 14:17:46 +0100

    Skrev färdigt optimize metod2

--- src/Supermarket/Main/Optimize.java
+++ src/Supermarket/Main/Optimize.java
@@ -12,9 +12,9 @@ import Supermarket.View.SupermarketView;
 
 public class Optimize {
      public static void main(String [] args){
-          int result = findMinRegistersSeed(0, 10.00, 5, 1.0,
-                  1234, 2.0, 3.0, 0.5, 1.0);
-          System.out.println("Result: " + result);
+          int result = findMinRegistersSeed(0, 8.00, 7, 3.0,
+                  13, 0.35, 0.6, 0.6, 0.9);
+          System.out.println("Smallest amount of registers yielding least missed customers: "  + result);
      }
 
      public static SupermarketState runSimulationOnce(
@@ -45,9 +45,23 @@ public class Optimize {
                                                   double pMax) {
           int counter = 0;
           int numberOfRegisters = 0;
+          int bestAmountOfRegisters = 0;
+          int lowestMissed = runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+                  lambda, seed, kMin, kMax, pMin, pMax).getMissedCustomers();
           while(counter < 10){
-
+               if(runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+                       lambda, seed, kMin, kMax, pMin, pMax).getMissedCustomers() < lowestMissed){
+                    lowestMissed = runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+                            lambda, seed, kMin, kMax, pMin, pMax).getMissedCustomers();
+                    bestAmountOfRegisters = runSimulationOnce(openingTime, closingTime, maxCustomers, numberOfRegisters,
+                            lambda, seed, kMin, kMax, pMin, pMax).getOpenRegisters();
+                    counter = 0;
+               }
+               else{
+                    counter++;
+               }
+               numberOfRegisters++;
           }
-          return 0;
+          return bestAmountOfRegisters;
      }
 }
